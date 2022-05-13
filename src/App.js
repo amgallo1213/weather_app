b4734dd8e8b4621cba48f996c835b607401781d4
Andrea Gallo <agallo1213@gmail.com>
2022-05-12 23:37:54 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       
         <h1>Mom's Weather App</h1>
-        <p>created by Your Youngest</p>
+        <p>&#x1F63B; created by Your Youngest &#x1F63B;</p>
         <main>
           <Forecast />
         </main>
