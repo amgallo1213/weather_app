6e566a99f50e417f5a2a5313a5d6fc8afd7d9ab6
Andrea Gallo <agallo1213@gmail.com>
2022-05-12 23:26:58 -0400

    small updates

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Forecast from './Forecast/Forecast';
 function App() {
   return (
     <div className="App">
-      <h1>Mom's Weather App</h1>
-      <p>created by Your Youngest</p>
-      <main>
-        <Forecast />
-      </main>
+      
+        <h1>Mom's Weather App</h1>
+        <p>created by Your Youngest</p>
+        <main>
+          <Forecast />
+        </main>
+      
     </div>
   );
 }
--- src/Conditions/Conditions.js
+++ src/Conditions/Conditions.js
@@ -12,11 +12,11 @@ const Conditions = (props) => {
                 <div>
                     <div>
                         <h2><strong>{props.responseObj.name}</strong></h2>
-                        <h4>It is currently <h3>{Math.round(props.responseObj.main.temp)}</h3> degrees out with {props.responseObj.weather[0].description}.</h4>
+                        <h4>It is currently <h3>{Math.round(props.responseObj.main.temp)} &#176;</h3> with {props.responseObj.weather[0].description}.</h4>
                     </div>
                     <div className="HighLow">
-                        <p><strong>High: {Math.round(props.responseObj.temp_max)}</strong></p>
-                        <p><strong>Low: {Math.round(props.responseObj.temp_min)}</strong></p>
+                        <p><strong>High: {Math.round(props.responseObj.main.temp_max)}</strong></p>
+                        <p><strong>Low: {Math.floor(props.responseObj.main.temp_min)}</strong></p>
                     </div>
                 </div>
                 : null
